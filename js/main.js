8c544abbd0c3cb9dec54264de847e16a96aaeab5
avreeeb <burn0243@algonquinlive.com>
2015-02-25 17:20:28 -0500

    js working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 // JavaScript Document
 
-alert('Hello');
+var $form = $('form');
+var $input = $('#the-color');
+
+$form.on('submit', function (eventObject) {
+	//Stop the form from doing what it normally does
+	eventObject.preventDefault();
+	
+	var $theH1 = $('<h1>');
+	$theH1.html($input.val());
+	
+	$form.append($theH1);
+	
+	$input.val('');
+});
+	
+
+
 
 
